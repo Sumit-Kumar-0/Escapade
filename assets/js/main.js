0655ccd09e9e031b3f7316d1c1a416811c4ee27f
Sumit Kumar <sumitkabox@gmail.com>
2023-06-02 22:06:53 +0530

    no change

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,9 @@ document.addEventListener("scroll", () => {
     header.classList.remove("scroll-class");
   }
 })
+
+
+
 // responsive menu on click 
 let respMenu = document.querySelector(".header");
 respMenu.addEventListener("click", ()=>{
